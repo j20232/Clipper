7d523a1ee16d82b881a4dd512cc084ec3198f17f
mocobt <mocobt@gmail.com>
2019-10-05 16:20:43 +0900

    change the time

--- popup.js
+++ popup.js
@@ -113,7 +113,7 @@ document.addEventListener('DOMContentLoaded', () => {
       // hide popup automatically
       setTimeout(function () {
         window.close();
-      }, 300000);
+      }, 3000);
     });
   });
 });
